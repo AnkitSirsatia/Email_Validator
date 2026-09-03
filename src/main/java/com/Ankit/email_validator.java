d6c75027b7e44023003cba83fce23a7757e646e2
Ankit Sirsatia <sirsatiaa@gmail.com>
2026-09-04 03:57:45 +0530

    Initial commit

---
+++ src/main/java/com/Ankit/email_validator.java
@@ -0,0 +1,34 @@
+package com.Ankit;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.InitialDirContext;
+
+
+public class email_validator {
+
+    public static boolean MXLookup( String domain) throws NamingException{
+        InitialDirContext context = new InitialDirContext();
+        Attributes attributes = context.getAttributes("dns:/" + domain, new String[]{"MX"});
+        return attributes.get("MX") != null;
+    }
+
+    public static boolean emailValidator(String email){
+        if(email.contains("@@") || email.matches(".*[^a-zA-Z0-9@.].*") || email.contains("..") || email.contains(".@")
+                || email.contains("@.")){
+            return false;
+        }else {
+            if( email.matches("[a-zA-Z0-9@.]+") && email.contains("@"))
+            {
+                int atIndex = email.indexOf("@");
+                String domain = email.substring(atIndex + 1);
+                try{
+                return MXLookup(domain);
+                }catch(NamingException e){
+                    return false;
+                }
+            }else {
+                return false;
+            }
+        }
+    }
+}
